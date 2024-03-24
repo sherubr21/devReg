725f0164061fcce3fabfecc7650469cbb6f1b58f
Rigden23 <RigdenNamgay23@gmail.com>
2024-03-24 18:54:23 +1300

    27032024

--- src/main/java/com/Athang/Athang/controller/TraineeController.java
+++ src/main/java/com/Athang/Athang/controller/TraineeController.java
@@ -4,6 +4,7 @@ package com.Athang.Athang.controller;
 import com.Athang.Athang.model.Trainee;
 import com.Athang.Athang.service.TraineeService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
@@ -24,4 +25,23 @@ public class TraineeController {
     public List<Trainee> list() {
         return traineeService.getTrainee();
     }
+
+   @PutMapping("/update/{id}")
+  //  public String pem(String id){
+  // public ResponseEntity<Trainee> update(@PathVariable Integer id, @RequestBody Trainee trainee)
+   public ResponseEntity<Trainee> update(@PathVariable Integer id,@RequestBody Trainee trainee){
+       Trainee updatedTrainee = traineeService.updateTrainee(id, trainee);
+       if (updatedTrainee != null) {
+           return ResponseEntity.ok(updatedTrainee); // Return 200 OK with the updated trainee
+       } else {
+           return ResponseEntity.notFound().build(); // Return 404 Not Found if trainee with given ID not found
+       }
+      // return "kkk";
+   }
+  /*  @GetMapping("/pp")
+    public String test(){
+        return "kkk";
+    }*/
+
+
 }
